f0329858306f6b02a9f55f174949695eaecff631
developerphanom12 <developer.phanomprofessionls@gmail.com>
2024-10-07 18:31:34 +0530

    last changes

--- src/adminlogin/Admin.jsx
+++ src/adminlogin/Admin.jsx
@@ -104,9 +104,7 @@ function Admin() {
                   Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed
                   do eiusmod tempor incididunt ut labore et dolore magna aliqua.
                   Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                  laboris nisi ut aliquip ex ea commodo consequat.Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed
-                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-               
+                  laboris nisi ut aliquip ex ea commodo consequat.
                 </p>
               </div>
             </div>
